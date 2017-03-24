f4599d7194dadb7664ca823622780958250ccad3
DKing <DKingCN@users.noreply.github.com>
2017-03-24 17:25:05 +0800

    Update install_libsodium.sh

--- libsodium/install_libsodium.sh
+++ libsodium/install_libsodium.sh
@@ -3,17 +3,18 @@
 /bin/mkdir -p '/usr/local/lib'
 bin/mkdir -p '/usr/local/lib/pkgconfig'
 
+/usr/bin/install -c .libs/libsodium.so.18.1.0 /usr/local/lib/libsodium.so.18.1.0
 /usr/bin/install -c -m 644  sodium/core.h sodium/crypto_aead_aes256gcm.h sodium
 /usr/bin/install -c -m 644  sodium.h '/usr/local/include/.'
 /usr/bin/install -c   libsodium.la '/usr/local/lib'
 /usr/bin/install -c .libs/libsodium.so /usr/local/lib/libsodium.so.18.1.0
-/usr/bin/install -c .libs/libsodium.lai /usr/local/lib/libsodium.la
+/usr/bin/install -c .libs/libsodium.lai /usr/local/lib/libsodium.lai
 /usr/bin/install -c .libs/libsodium.a /usr/local/lib/libsodium.a
 /usr/bin/install -c -m 644 libsodium.pc '/usr/local/lib/pkgconfig'
 
-
-cd /usr/local/lib && { ln -s -f libsodium.so.18.1.0 libsodium.so.18 || { rm -f libsodium.so.18 && ln -s libsodium.so.18.1.0 libsodium.so.18; }; }
-cd /usr/local/lib && { ln -s -f libsodium.so.18.1.0 libsodium.so || { rm -f libsodium.so && ln -s libsodium.so.18.1.0 libsodium.so; }; }
+cd /usr/local/lib
+ln -s -f libsodium.so.18.1.0 libsodium.so.18
+ln -s -f libsodium.so.18.1.0 libsodium.so
 chmod 644 /usr/local/lib/libsodium.a
 ranlib /usr/local/lib/libsodium.a
 ldconfig -n /usr/local/lib
